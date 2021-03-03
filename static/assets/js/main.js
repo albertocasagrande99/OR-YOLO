e8098c899c390742a2362c30767d7cf7d7de62a6
albertocasagrande99 <alberto.casagrande@studenti.unitn.it>
2021-03-03 18:59:53 +0100

    Image detector 1.1

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -15,10 +15,12 @@ $(function () {
         var scroll = $(window).scrollTop();
         if (scroll < 20) {
             $(".navbar-area").removeClass("sticky");
-            $(".navbar .navbar-brand img").attr("src", "assets/images/logo.svg");
+            $(".navbar .navbar-brand img").attr("src", "static/assets/images/logo.svg");
+            $(".navbar .navbar-brand img").css("width", "60%");
         } else {
             $(".navbar-area").addClass("sticky");
-            $(".navbar .navbar-brand img").attr("src", "assets/images/logo-2.svg");
+            $(".navbar .navbar-brand img").attr("src", "static/assets/images/logo-2.svg");
+            $(".navbar .navbar-brand img").css("width", "40%");
         }
     });
 
